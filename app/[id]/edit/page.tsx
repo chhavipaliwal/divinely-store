761031374a6e0f4e9f9cf4eb62e6ec336bc26eae
Bhuneshvar <009ankitkalirawana@gmail.com>
2025-03-04 18:16:55 +0530

    Remove unused category fetching in edit link page
    - Delete getCategories function from edit link page
    - Remove category import from interface
    - Simplify edit link page rendering

--- app/[id]/edit/page.tsx
+++ app/[id]/edit/page.tsx
@@ -1,6 +1,6 @@
 import EditLink from '@/components/dashboard/links/EditLink';
 import { API_BASE_URL } from '@/lib/config';
-import { Category, Link } from '@/lib/interface';
+import { Link } from '@/lib/interface';
 import { cookies } from 'next/headers';
 import React from 'react';
 
@@ -20,19 +20,8 @@ const getLink = async (id: string) => {
   return data;
 };
 
-const getCategories = async () => {
-  const res = await fetch(`${API_BASE_URL}/category`, {
-    cache: 'no-cache',
-    method: 'GET',
-    headers: { Cookie: cookies().toString() }
-  });
-  const data = await res.json();
-  return data;
-};
-
 export default async function Page({ params }: Props) {
   const link: Link = await getLink(params.id);
-  const categories: Category[] = await getCategories();
   return (
     <>
       <EditLink link={link} />
